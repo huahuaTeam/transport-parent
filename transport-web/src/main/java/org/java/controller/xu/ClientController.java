9abf3f383cd4ce20631d193ad80372a0dee37107
xuzhuo <971359322@qq.com>
2018-09-19 10:44:16 +0800

    xxxx

--- transport-web/src/main/java/org/java/controller/xu/ClientController.java
+++ transport-web/src/main/java/org/java/controller/xu/ClientController.java
@@ -1,8 +1,11 @@
-package org.java.controller;
+package org.java.controller.xu;
 
 import org.java.mapper.xu.TestMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("client")
@@ -10,8 +13,19 @@ public class ClientController{
 
     @RequestMapping("/")
     public String client(){
-
         return "/xu/client";
+    }
+
+    @RequestMapping("add")
+    public String add(){
+        System.out.println("");
+        return "/xu/addClient";
+    }
+
+    @RequestMapping("addClient")
+    public String addClient(Map<String ,String> client){
+
+        return "/xu/addClient";
     }
 
 
